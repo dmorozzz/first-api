fce5fa804f36795cbdae79aa0bdb1df39026ce2e
morozzzDima <dmytromorozzz@gmail.com>
2020-11-15 21:45:34 +0200

    close branch

--- www/server.js
+++ www/server.js
@@ -1,9 +1,13 @@
 const { createServer } = require('http');
-const app = require('../src/app.js');
+const app = require('../api/app.js');
 
 const server = createServer(app);
+const dataBase = require('../database/mongodb.js');
 
-const port = process.env.PORT || 3000  ;
+const port = process.env.PORT || 3000;
+
+dataBase.once('open', () => console.log('db connection open'));
+dataBase.on('error', error => console.log(error));
 
 server.listen(port, () => {
     console.log(`server is running on port ${port}`);
