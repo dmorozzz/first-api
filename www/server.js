a5a52017fe51467a9b5afe4a3a1cfa9bfe9fe714
morozzzDima <dmytromorozzz@gmail.com>
2020-11-15 23:08:27 +0200

    configure mongoDB

--- www/server.js
+++ www/server.js
@@ -6,9 +6,16 @@ const dataBase = require('../database/mongodb.js');
 
 const port = process.env.PORT || 3000;
 
-dataBase.once('open', () => console.log('db connection open'));
-dataBase.on('error', error => console.log(error));
+const startServer = () => {
+    server.listen(port, () => {
+        console.log(`server is running on port ${port}`);
+    });
+}
 
-server.listen(port, () => {
-    console.log(`server is running on port ${port}`);
+dataBase.once('open', startServer);
+dataBase.on('error', error => { 
+    throw error;
 });
+
+
+
